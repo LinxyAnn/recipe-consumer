f4c796f1f22e132b0d179115b431d7e11a10512e
Ann Severyna <linxyann@gmail.com>
2020-03-22 06:59:39 +0200

    First commit

---
+++ src/main/java/com/larecette/recipeconsumer/RecipeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.larecette.recipeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeConsumerApplication.class, args);
+    }
+
+}
