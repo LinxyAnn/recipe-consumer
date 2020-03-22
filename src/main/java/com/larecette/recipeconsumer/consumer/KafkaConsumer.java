0e55dca26369d4d5da7bac71053afbdb02cf9c10
Ann Severyna <linxyann@gmail.com>
2020-03-22 08:08:56 +0200

    Consume Entity

--- src/main/java/com/larecette/recipeconsumer/consumer/KafkaConsumer.java
+++ src/main/java/com/larecette/recipeconsumer/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.larecette.recipeconsumer.consumer;
 
+import com.larecette.recipeconsumer.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,10 @@ public class KafkaConsumer {
     @KafkaListener(topics = "KafkaExampleString", groupId = "group_id")
     public void consume(String message) {
         log.info("Consumed message: " + message);
+    }
+
+    @KafkaListener(topics = "KafkaExampleOne", groupId = "group_json", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        log.info("Consumed JSON message: " + user.toString());
     }
 }
--- src/main/java/com/larecette/recipeconsumer/model/User.java
+++ src/main/java/com/larecette/recipeconsumer/model/User.java
@@ -2,8 +2,10 @@ package com.larecette.recipeconsumer.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private String name;
